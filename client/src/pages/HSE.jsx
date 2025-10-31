6854d5994237f8db587ea60f1867119406c39146
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-10-31 17:02:48 +0530

    40th  commit

--- client/src/pages/HSE.jsx
+++ client/src/pages/HSE.jsx
@@ -24,15 +24,15 @@ const HSE = () => {
                 </div>
 
                 <div className="relative w-full overflow-hidden -mt-20">
-                    <div className="flex animate-scroll gap-[2px] lg:gap-[24px] mx-auto min-w-max">
-                        <img src={c1} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
-                        <img src={c2} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
-                        <img src={c3} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
-                        <img src={c4} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
-                        <img src={c1} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
-                        <img src={c2} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
-                        <img src={c3} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
-                        <img src={c4} className="w-[36vw] h-[24vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                    <div className="flex animate-scroll gap-[2px] lg:gap-[24px] min-w-full">
+                        <img src={c1} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                        <img src={c2} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                        <img src={c3} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                        <img src={c4} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                        <img src={c1} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                        <img src={c2} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                        <img src={c3} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
+                        <img src={c4} className="w-[36vw] h-[26vh] lg:w-[18vw] lg:h-[47vh] flex-shrink-0" />
                     </div>
                 </div>
 
