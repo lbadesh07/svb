f7c625d21b4ebb77b52848c18bdba64000eebef7
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-11-02 01:33:27 +0530

    57th  commit

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,7 +3,7 @@ import LandingBanner from "./LandingBanner";
 import constructionImg from "../assets/cap.png";
 import pillarImg from "../assets/pillarsOfTrustIMG.png"
 import "../CSS/Landing.css"
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import fleet1 from "../assets/fleet-img1.png"
 import fleet2 from "../assets/fleet-img2.png"
 import Cards from "./Cards";
@@ -19,44 +19,44 @@ import c9 from "../assets/globalCard9.png"
 import c10 from "../assets/globalCard10.png"
 
 const Landing = () => {
-   const [isMobile, setIsMobile] = useState(false);
-  
-      const handleResize = () => {
-          const width = window.innerWidth;
-  
-          if (width < 740) {
-              setIsMobile(true);
-          } else {
-              setIsMobile(false);
-          }
-      };
-  
-      useEffect(() => {
-          handleResize(); // Run once when mounted
-          window.addEventListener("resize", handleResize);
-  
-          return () => window.removeEventListener("resize", handleResize);
-      }, []);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+
+    if (width < 740) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize(); // Run once when mounted
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <div className="w-screen ">
       {/* <LandingBanner></LandingBanner> */}
       <div id="landingBanner" className="w-screen h-[45vh] lg:h-[88vh]">
         <div className="lg:pt-[120px] lg:pl-[110px] pl-[40px] pt-[60px]">
 
-        <div className="flex flex-col  gap-[10px] lg:gap-[16px]">
-          <p className="text-[1.8rem] lg:text-[4rem] text-white aboutCom font-bold leading-8 lg:leading-19">Building the Future with <br /> <span className="text-[#FDC000]">Strength & Precision</span> </p>
-          {isMobile?<p className="text-white w-[88%]  text-[1rem] lg:text-[20px] popins">Delivering reliable infrastructure solutions  with modern machinery  and  expert execution.</p>:
-          <p className="text-white text-[1.3rem] popins">Delivering reliable infrastructure solutions with modern machinery <br /> and expert execution.</p>}
+          <div className="flex flex-col  gap-[10px] lg:gap-[16px]">
+            <p className="text-[1.8rem] lg:text-[4rem] text-white aboutCom font-bold leading-8 lg:leading-19">Building the Future with <br /> <span className="text-[#FDC000]">Strength & Precision</span> </p>
+            {isMobile ? <p className="text-white w-[88%]  text-[1rem] lg:text-[20px] popins">Delivering reliable infrastructure solutions  with modern machinery  and  expert execution.</p> :
+              <p className="text-white text-[1.3rem] popins">Delivering reliable infrastructure solutions with modern machinery <br /> and expert execution.</p>}
+          </div>
+          <button className="lg:w-[10vw] lg:h-[7vh]  rounded-md text-[14px] px-3 lg:px-0 lg:py-0 py-2 lg:py-0  bg-[#FDC000] lg:rounded-md lg:text-[20px] popins-noweight font-semibold mt-[2.5rem]">See Our Fleet</button>
         </div>
-        <button className="lg:w-[10vw] lg:h-[7vh]  rounded-md text-[14px] px-3 lg:px-0 lg:py-0 py-2 lg:py-0  bg-[#FDC000] lg:rounded-md lg:text-[20px] popins-noweight font-semibold mt-[2.5rem]">See Our Fleet</button>
       </div>
-        </div>
-     
+
 
       <div className="flex flex-col  gap-[10vh] lg:gap-[20vh] my-15 lg:my-30  w-screen">
         <div>
-          <section className="flex  flex-row w-full mx-2 lg:justify-center  text-gray-800  ">
-          
+          <section className="flex  flex-row w-full mx-2 lg:mx-0 lg:justify-center  text-gray-800  ">
+
             <div className="">
               <img
                 src={constructionImg}
@@ -65,15 +65,15 @@ const Landing = () => {
               />
             </div>
 
-           
-            <div className="lg:w-[33vw] w-[50vw]  h-fit  flex flex-col">
-              <p className="popins lg:leading-16 text-[1.2rem] leading-7  lg:p-4 rounded-bl-2xl -ml-20 p-2 lg:-ml-56 bg-white  text-black lg:text-[4rem] font-semibold">
+
+            <div className="lg:w-[33vw] w-[50vw]  h-fit  flex flex-col ">
+              <p className="fontMon lg:leading-16 text-[1.5rem] leading-7  lg:p-4 rounded-bl-2xl -ml-20 p-2 lg:-ml-56 bg-white  text-[#333333] lg:text-[4rem] font-semibold">
                 Great Relationships,
                 <br /> Great <span></span>
                 <span className="text-[#FDC000]">Infrastructure</span>
               </p>
               <div className="flex flex-col place-items-end">
-                <p className=" text-[#4D4D4D]  lg:w-[32vw] text-[0.6rem] lg:text-[1.25rem] px-2 pt-2 lg:pl-5 lg:pt-10  ">
+                <p className=" text-[#4D4D4D] popins lg:w-[32vw] text-[0.6rem] lg:text-[1.25rem] px-2 pt-2 lg:pl-5 lg:pt-10  ">
                   <span className="font-semibold lg:mr-1">SVB Infra Projects</span>
                   is a 3rd generation family-run business. SVB has been managing
                   earthmoving projects since 2003. Based in Bangalore, we provide
@@ -85,13 +85,14 @@ const Landing = () => {
                   some of the biggest names in construction and civil engineering
                   sector.
                 </p>
-                <button className="popins w-[18vw] mr-3 text-[10px] lg:text-[20px]  lg:w-[9vw] bg-[#FDC000]  py-2 rounded-md ">
-                  Know More
+                <button
+                  className="popins  w-[calc(19vw+0px)] lg:w-[calc(9vw+0px)] mr-3 text-[calc(0.8rem+0px)] lg:text-[calc(1.3rem+0px)] bg-[#FDC000] py-[calc(0.5rem+0px)] rounded-md
+             ">Know More
                 </button>
               </div>
             </div>
           </section>
-          <div className="flex gap-5 my-4  lg:my-10 pl-[4vw] lg:pl-[9vw]">
+          <div className="flex gap-5 my-6  lg:my-10 pl-[4vw] lg:pl-[9vw]">
             <div className="border lg:border-2 border-[#FDC000] rounded-md flex lg:w-[12vw] w-[27vw] justify-evenly h-[5vh]   lg:h-[11vh] place-items-center">
               <p className="popins text-lg lg:text-3xl font-semibold ">50+</p>
               <p className="popins font-medium text-[9px] lg:text-[15px] text-[#4D4D4D]  text-start">Successfull <br /> Project</p>
@@ -128,7 +129,7 @@ const Landing = () => {
                 <img src={fleet1} className="lg:w-[18vw] lg:h-[43vh] w-[46vw] h-[20vh]" />
                 <img src={fleet2} className="lg:w-[18vw] lg:h-[43vh] w-[46vw] h-[20vh]" />
 
-        
+
                 <img src={fleet1} className="lg:w-[18vw] lg:h-[43vh] w-[46vw] h-[20vh]" />
                 <img src={fleet2} className="lg:w-[18vw] lg:h-[43vh] w-[46vw] h-[20vh]" />
                 <img src={fleet1} className="lg:w-[18vw] lg:h-[43vh] w-[46vw] h-[20vh]" />
@@ -150,22 +151,22 @@ const Landing = () => {
             <button className="lg:ml-8 ml-4 popins-bold text-[9px] lg:text-[17px]  lg:w-[10vw] w-[15vw] bg-[#FDC000] py-1  lg:py-3 rounded-sm lg:rounded-md mt-3  lg:mt-15">Explore All</button>
           </div>
           <div className=" flex w-full py-2 lg:py-6 relative ">
-            {isMobile? <p className="text-[#4D4D4D] w-[40vw]  popins  text-[8px] lg:text-[16px] lg:mt-5 font-semibold ">We have successfully completed a wide range of  earthwork and excavation projects for leading  clients across Karnataka. Our portfolio includes  residential, commercial, and infrastructure  developments delivered with quality and precision.</p>:
-            <p className="text-[#4D4D4D] popins  text-[8px] lg:text-[16px] lg:mt-5 font-semibold ">We have successfully completed a wide range of <br /> earthwork and excavation projects for leading <br /> clients across Karnataka. Our portfolio includes <br /> residential, commercial, and infrastructure <br /> developments delivered with quality and precision.</p>}
+            {isMobile ? <p className="text-[#4D4D4D] w-[40vw]  popins  text-[8px] lg:text-[16px] lg:mt-5 font-semibold ">We have successfully completed a wide range of  earthwork and excavation projects for leading  clients across Karnataka. Our portfolio includes  residential, commercial, and infrastructure  developments delivered with quality and precision.</p> :
+              <p className="text-[#4D4D4D] popins  text-[8px] lg:text-[16px] lg:mt-5 font-semibold ">We have successfully completed a wide range of <br /> earthwork and excavation projects for leading <br /> clients across Karnataka. Our portfolio includes <br /> residential, commercial, and infrastructure <br /> developments delivered with quality and precision.</p>}
             <div className="absolute  left-[42vw] lg:left-[39vw] -top-[11vw] lg:-top-[7vw] lg:w-[50vw] py-2 lg:py-4  overflow-hidden ">
-         
+
               <div className="flex lg:gap-x-6 gap-x-3 animate-scroll">
-              
-               <img src={c1} alt="" className="w-[17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c2} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c3} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c4} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c5} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c6} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c7} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c8} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c9} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
-               <img src={c10} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+
+                <img src={c1} alt="" className="w-[17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c2} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c3} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c4} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c5} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c6} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c7} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c8} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c9} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
+                <img src={c10} alt="" className="w-17vw] h-[10vh] lg:w-[18vw] lg:h-[47vh]" />
 
               </div>
             </div>
@@ -173,11 +174,11 @@ const Landing = () => {
 
         </div>
         <div id="landing-contact" className="lg:w-[78vw] w-[95vw]  text-white lg:h-[37vh]  mx-auto flex  place-items-center justify-evenly gap-17 lg:gap-70">
-            <div>
-              <p className="text-[0.8rem] lg:text-[1.5rem] popins-bold ">Let's Build Your Next Project Together</p>
-              <p className="popins-bold text-[1.2rem] lg:text-[3.5rem]">Contact With Us!</p>
-            </div>
-           <button className="lg:w-[9vw] w-[16vw] rounded-sm text-[10px] lg:text-[16px] h-[2vh] lg:h-[6vh] bg-[#FDC000] lg:rounded-lg">Contact Now</button>
+          <div>
+            <p className="text-[0.8rem] lg:text-[1.5rem] popins-bold ">Let's Build Your Next Project Together</p>
+            <p className="popins-bold text-[1.2rem] lg:text-[3.5rem]">Contact With Us!</p>
+          </div>
+          <button className="lg:w-[9vw] w-[16vw] rounded-sm text-[10px] lg:text-[16px] h-[2vh] lg:h-[6vh] bg-[#FDC000] lg:rounded-lg">Contact Now</button>
         </div>
       </div>
     </div>
