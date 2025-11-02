95f2ba79624da1139758f1a2734d47ab06700d37
Be-A-Coder01 <adeshmendekar07@gmail.com>
2025-11-02 10:40:04 +0530

    74th  commit

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -185,13 +185,13 @@ const Landing = () => {
         </div>
         <div id="landing-contact" className="lg:w-[78vw] w-[95vw] px-3 lg:px-0  text-white lg:h-[37vh]  mx-auto flex  place-items-center justify-evenly gap-[4.2rem] lg:gap-70">
           <div className="">
-            <p className="text-[0.8rem] lg:text-[1.5rem] popins-bold ">Let's Build Your Next Project Together</p>
-            <p className="fontMon font-bold text-[1.2rem] lg:text-[3.5rem]">Contact With Us!</p>
+            <p className="text-[0.7rem] lg:text-[1.5rem] popins-bold ">Let's Build Your Next Project Together</p>
+            <p className="fontMon font-bold text-[1.3rem] lg:text-[3.5rem]">Contact With Us!</p>
           </div>
           <button
             className="lg:w-[calc(9vw+0px)] w-[calc(16vw+0px)] 
              rounded-sm lg:rounded-lg 
-             text-[calc(9px+0px)] lg:text-[calc(16px+0px)] 
+             text-[calc(0.8rem+0.3vw)] 
              h-[calc(3vh+0px)] lg:h-[calc(6vh+0px)] 
              bg-[#FDC000] ">
             Contact Now
